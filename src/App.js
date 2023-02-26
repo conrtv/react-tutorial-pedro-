53a0a7e9407122bdc60b643c919ca3358ed0f913
conrtv <38775548+conrtv@users.noreply.github.com>
2023-02-25 19:59:04 -0600

    ep7ex3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import './App.css';
-import Axios from 'axios';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [catFact, setCatFact] = useState('');
-  const fetchCatFact = () => {
-    Axios.get('https://catfact.ninja/fact').then((res) => {
-      setCatFact(res.data.fact);
-    });
+  //    `https://excuser-three.vercel.app/v1/excuse/${category}`
+
+  const [genExcuse, setGenExcuse] = useState(null);
+
+  const fetchExcuse = (excuse) => {
+    axios
+      .get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`)
+      .then((res) => {
+        setGenExcuse(res.data[0].excuse);
+      });
   };
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
 
   return (
     <div className="App">
-      <button onClick={fetchCatFact}> Generate Cat Fact</button>
-      <p>{catFact}</p>
+      <h1>Generate An Excuse</h1>
+
+      <button onClick={() => fetchExcuse("family")}>Family</button>
+      <button onClick={() => fetchExcuse("office")}>Office</button>
+      <button onClick={() => fetchExcuse("children")}>Children</button>
+      <button onClick={() => fetchExcuse("college")}>College</button>
+      <button onClick={() => fetchExcuse("party")}>Party</button>
+      <button onClick={() => fetchExcuse("funny")}>Funny</button>
+      <button onClick={() => fetchExcuse("unbelievable")}>Unbelievable</button>
+      <button onClick={() => fetchExcuse("developers")}>Developers</button>
+      <button onClick={() => fetchExcuse("gaming")}>Gaming</button>
+
+      <p> {genExcuse}</p>
     </div>
   );
 }
-
 export default App;
